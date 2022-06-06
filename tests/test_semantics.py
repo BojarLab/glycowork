29f6925bf4f000e855be45ddfc499549d5625b71
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-06-06 16:06:07 +0200

    Add tests for standardized semantics

---
+++ tests/test_semantics.py
@@ -0,0 +1,38 @@
+import unittest
+
+import bioregistry
+
+from glycowork.glycan_data.loader import build_custom_df, df_glycan
+
+
+class TestSemantics(unittest.TestCase):
+    def setUp(self) -> None:
+        self.df = build_custom_df(df_glycan, kind="df_tissue")
+
+    def test_tissue_ids(self):
+        """Test that entries in the tissue_id column are valid CURIEs."""
+        for tissue_id in sorted(self.df["tissue_id"].unique()):
+            with self.subTest(tissue_id=tissue_id):
+                self.assertIn(
+                    ":",
+                    tissue_id,
+                    msg=f"Tissue identifier not written as a CURIE: {tissue_id}",
+                )
+                prefix, identifier = tissue_id.split(":")
+                self.assertIsNotNone(
+                    bioregistry.normalize_prefix(prefix),
+                    msg="prefix is not registered in the Bioregistry",
+                )
+                pp = bioregistry.get_preferred_prefix(prefix)
+                self.assertEqual(
+                    pp,
+                    prefix,
+                    msg=f"prefix is not standardized with the Bioregistry. Should be: {pp}",
+                )
+                pattern = bioregistry.get_pattern(prefix)
+                self.assertIsNotNone(pattern, msg="")
+                self.assertRegex(
+                    identifier,
+                    pattern,
+                    msg=f"local unique identifier does not match standard pattern from the Bioregistry: {pattern}",
+                )
