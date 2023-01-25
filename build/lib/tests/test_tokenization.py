93c6b809a625cae281eee148f6b461edb95aa743
Kathryn <kklarich@renagadetx.com>
2023-01-25 13:51:30 -0700

    update imports

--- build/lib/tests/test_tokenization.py
+++ build/lib/tests/test_tokenization.py
@@ -2,7 +2,6 @@ import glycowork
 
 from glycowork.motif.tokenization import *
 from glycowork.motif.graph import glycan_to_nxGraph
-from glycowork.motif.tokenization import cfg_to_iupac
 
 glycans = ['Man(a1-3)[Man(a1-6)][Xyl(b1-2)]Man(b1-4)GlcNAc(b1-4)[Fuc(a1-3)]GlcNAc',
            'Man(a1-2)Man(a1-2)Man(a1-3)[Man(a1-3)Man(a1-6)]Man(b1-4)GlcNAc(b1-4)GlcNAc',
