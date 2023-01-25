8499d625f53deec6ee6db4b69890f94ef6050e9f
Kathryn <kklarich@renagadetx.com>
2023-01-25 12:50:25 -0700

    Update tests

--- setup.py
+++ setup.py
@@ -26,5 +26,5 @@ setuptools.setup(
                       "seaborn", "xgboost", "mpld3",
                       "requests", "pandas", "glyles",
                       "pubchempy", "matplotlib-inline",
-                      "python-louvain", "drawSvg", "pytest"],
+                      "python-louvain", "drawSvg"],
 )
