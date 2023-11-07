941855e147278e1a4d13debe87830c11df1f8729
Daniel Bojar <daniel@bojar.net>
2023-11-07 12:21:09 +0100

    extend WURCS support

--- glycowork/motif/processing.py
+++ glycowork/motif/processing.py
@@ -463,9 +463,11 @@ def wurcs_to_iupac(wurcs):
   """
   wurcs = wurcs[wurcs.index('/')+1:]
   monosaccharide_mapping = {
-    'a2122h-1b_1-5_2*NCC/3=O': 'GlcNAcb',
-    'a1122h-1b_1-5': 'Manb',
-    'a1122h-1a_1-5': 'Mana'
+    'a2122h-1b_1-5_2*NCC/3=O': 'GlcNAcb', 'a2112h-1a_1-5_2*NCC/3=O': 'GalNAca',
+    'a1122h-1b_1-5': 'Manb', 'Aad21122h-2a_2-6_5*NCC/3=O': 'Neu5Aca',
+    'a1122h-1a_1-5': 'Mana', 'a2112h-1b_1-5': 'Galb', 'Aad21122h-2a_2-6_5*NCCO/3=O': 'Neu5Gca',
+    'a2112h-1b_1-5_2*NCC/3=O_?*OSO/3=O/3=O': 'GalNAcOSb', 'a2112h-1b_1-5_2*NCC/3=O': 'GalNAcb',
+    'a1221m-1a_1-5': 'Fuca', 'a2122h-1b_1-5_2*NCC/3=O_6*OSO/3=O/3=O': 'GlcNAc6Sb'
     }
   parts = wurcs.split('/')
   topology = parts[-1].split('_')
@@ -480,20 +482,21 @@ def wurcs_to_iupac(wurcs):
     target_index, target_carbon = connectivity[target[0]], target[1:]
     source_mono = monosaccharide_mapping[monosaccharides[int(source_index)-1]]
     target_mono = monosaccharide_mapping[monosaccharides[int(target_index)-1]]
-    iupac_parts.append(f"{target_mono}({target_carbon}-{source_carbon}){source_mono}")
-  iupac = iupac_parts[0]
-  i = 2
-  for parts in iupac_parts[1:]:
+    iupac_parts.append((f"{target_mono}({target_carbon}-{source_carbon}){source_mono}", target[0], source[0]))
+  iupac_parts = sorted(iupac_parts, key=lambda x: x[2])
+  iupac = iupac_parts[0][0]
+  for parts, tgt, src in iupac_parts[1:]:
     overlap = parts.split(')')[-1]
     idx = iupac.index(overlap)
-    prefix = '[' if degrees[chr(97 + i)] == 1 else ''
-    suffix = ']' if degrees[chr(97 + i - 2)] > 2 else ''
+    prefix = '[' if degrees[tgt] == 1 else ''
+    suffix = ']' if (degrees[src] > 2) or (degrees[src] == 2 and src =='a') else ''
     iupac = iupac[:idx] + prefix + parts.split(')')[0]+')' + suffix + iupac[idx:]
-    i += 1
   iupac = iupac[:-1]
   pattern = re.compile(r'([ab\?])\(')
   iupac = pattern.sub(lambda match: f"({match.group(1)}", iupac)
   iupac = iupac.strip('[]')
+  if ']' in iupac and iupac.index(']') < iupac.index('['):
+    iupac = iupac.replace(']', '', 1)
   return iupac
 
 
