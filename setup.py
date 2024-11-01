94e7bb5ae5d0d03c8f4cb3d87db4bbcac899ff13
Daniel Bojar <daniel@bojar.net>
2024-11-01 06:40:27 +0100

    include json in package data

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/BojarLab/glycowork",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    package_data={'': ['*.csv', '*.pkl', '*.jpg', '*.pt']},
+    package_data={'': ['*.csv', '*.pkl', '*.jpg', '*.pt', '*.json']},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
