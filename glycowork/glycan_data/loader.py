42f1d99cc37b0a25f6d08ca9b8953c1d1384c62f
Daniel Bojar <daniel@bojar.net>
2025-02-18 09:52:15 +0100

    exclude tests from coverage report

--- glycowork/glycan_data/loader.py
+++ glycowork/glycan_data/loader.py
@@ -252,7 +252,7 @@ def download_model(file_id: str, # Google Drive file ID
   "Download the model weights file from Google Drive"
   file_id = file_id.split('/d/')[1].split('/view')[0]
   url = f'https://drive.google.com/uc?id={file_id}'
-  response = requests.get(url, stream = True, timeout = 10)
+  response = requests.get(url, stream = True, timeout = 30)
   if response.status_code == 200:
     with open(local_path, 'wb') as f:
       for chunk in response.iter_content(chunk_size = 8192):
